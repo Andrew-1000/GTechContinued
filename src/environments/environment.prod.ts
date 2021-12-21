41e4274ccb5f4809b4168f1782abb916833a2b88
Andrew-1000 <andruambia95@gmail.com>
2021-12-21 11:37:58 +0300

    firebaseConfigurations added to environments file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,20 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig:{
+
+    apiKey: "AIzaSyBTaiIw1dfdG093204BKVHUurLy7Otu3BI",
+  
+    authDomain: "g-tech-inventory.firebaseapp.com",
+  
+    projectId: "g-tech-inventory",
+  
+    storageBucket: "g-tech-inventory.appspot.com",
+  
+    messagingSenderId: "369201313814",
+  
+    appId: "1:369201313814:web:053f282e4bdc51756fdd37",
+  
+    measurementId: "G-79DPDERWZD"
+  
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,24 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig:{
+
+    apiKey: "AIzaSyBTaiIw1dfdG093204BKVHUurLy7Otu3BI",
+  
+    authDomain: "g-tech-inventory.firebaseapp.com",
+  
+    projectId: "g-tech-inventory",
+  
+    storageBucket: "g-tech-inventory.appspot.com",
+  
+    messagingSenderId: "369201313814",
+  
+    appId: "1:369201313814:web:053f282e4bdc51756fdd37",
+  
+    measurementId: "G-79DPDERWZD"
+  
+  }
 };
 
 /*
