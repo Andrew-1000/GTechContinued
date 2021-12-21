47d2edd1c4426b8e510b31d98acf2cc2e2c7bb4c
Andrew-1000 <andruambia95@gmail.com>
2021-12-21 22:45:06 +0300

    SignIn, SignUp, ForgotPassword, SendVerification Email and login with googleauthentication methods added to project

--- src/app/services/authentication-service/auth.service.ts
+++ src/app/services/authentication-service/auth.service.ts
@@ -1,18 +1,130 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
+import { Employee } from 'src/app/model/employee';
+
+import * as firebase from 'firebase/app';
+import 'firebase/auth'
+import 'firebase/firestore'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  userData: any;
+  
+  
+  constructor(
+    private httpClient: HttpClient,
+    public afs: AngularFirestore,
+    public afAuth: AngularFireAuth,
+    public router: Router,
+    public ngZone: NgZone,
+    
+    ) { 
+      this.afAuth.authState.subscribe(employee => {
+        if (employee) {
+          this.userData = employee;
+          localStorage.setItem('employee', JSON.stringify(this.userData));
+          JSON.parse(localStorage.getItem('employee') || '{}');
+        } else {
+          localStorage.setItem('employee','null');
+          JSON.parse(localStorage.getItem('employee') || '{}');
+        }
+      })
+    }
 
-  private _registrationUrl = 'http://gtechwms.herokuapp.com/accounts/';
+    //Signing in With Username and Password - Firebase
+    async SignIn(email: any, password : any) {
+      try {
+        const result = await this.afAuth.signInWithEmailAndPassword(email, password);
+        this.ngZone.run(() => {
+          this.router.navigate(['dashboard']);
+        });
+        this.SetUserData(result);
+      } catch (error) {
+        window.alert(error);
+      }
+  }
 
-  // private _loginUrl = "https://gtechwms.herokuapp.com/accounts/login"
-  private _loginUrl = "#"
+  async Signup(email: any, password: any) {
+    try {
+      const result = await this.afAuth.createUserWithEmailAndPassword(email, password);
+      // this.SendVerificationMail();
+      this.SetUserData(result);
+    } catch (error) {
+      window.alert(error);
+    }
+  }
+  // async SendVerificationMail() {
+  //   this.afAuth.auth.sendEmailVerification();
+  //   this.router.navigate(['verify-email-address']);
+  // }
 
-  constructor(private httpClient: HttpClient) { }
+  //reset forgot password
+  async ForgotPassword(passwordResetEmail: any) {
+  try {
+    await this.afAuth.sendPasswordResetEmail(passwordResetEmail);
+    window.alert('Password reset sent to email, check your inbox.');
+  } catch (error) {
+    window.alert(error);
+  }
 
+}
+
+//Returns true when user is logged in and email is verified
+
+get isLoggedIn(): boolean {
+  const employee = JSON.parse(localStorage.getItem('user') || '{}')
+  return (employee !== null && employee.emailVerified !== false) ? true: false;
+
+}
+//Logging in with Google
+// GoogleAuth() {
+//   return this.AuthLogin(new auth.GoogleAuthProvider());
+// }
+//   async AuthLogin(provider: auth.GoogleAuthProvider) {
+//     try {
+//       const result = await this.afAuth.signInWithPopup(provider);
+//       this.ngZone.run(() => {
+//         this.router.navigateByUrl('/dashboard');
+//       });
+//       this.SetUserData(result);
+//     } catch (error) {
+//       window.alert(error);
+//     }
+//   }
+
+//Setting up data when signing in specicifically with username and password, sign up with user and password and sign in with social auth provider in firestore database using 
+// AngularFirestore + AngularFireStoreDocuemtn service
+  SetUserData(employee: any) {
+    const userRef: AngularFirestoreDocument<any> = this.afs.doc(`employee/${employee.employeeId}`);
+    const userData: Employee = {
+      employeeId: employee.employeeId,
+      employeeEmail: employee.employeeEmail,
+      employeeName: employee.employeeName,
+      emailVerified: employee.emailVerified
+    }
+    return userRef.set(userData, {
+      merge: true
+    })
+  }
+
+  //Sign Out
+  async SignOut() {
+    await this.afAuth.signOut();
+    localStorage.removeItem('employee');
+    this.router.navigate(['login']);
+  }
+
+    //Creating Account and Logging in Using HerokuApp API
+    private _registrationUrl = 'http://gtechwms.herokuapp.com/accounts/';
+
+    // private _loginUrl = "https://gtechwms.herokuapp.com/accounts/login"
+    private _loginUrl = "#"
+  
   registerUser(userData: any){
     return this.httpClient.post<any>(this._registrationUrl, userData)
   }
