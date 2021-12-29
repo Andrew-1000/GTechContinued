1a916127b6101cb7ca28cc119cd2db0de1ec24c1
Andrew-1000 <andruambia95@gmail.com>
2021-12-29 22:28:36 +0300

    environment commented

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  // production: false,
+  production: false,
   // firebaseConfig:{
 
   //   apiKey: "AIzaSyBTaiIw1dfdG093204BKVHUurLy7Otu3BI",
