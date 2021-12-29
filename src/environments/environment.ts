587117fcc78fa27b5ccf7a077366269c3f73801e
Andrew-1000 <andruambia95@gmail.com>
2021-12-29 22:27:51 +0300

    commented firebase configs for test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,24 +3,24 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebaseConfig:{
+  // production: false,
+  // firebaseConfig:{
 
-    apiKey: "AIzaSyBTaiIw1dfdG093204BKVHUurLy7Otu3BI",
+  //   apiKey: "AIzaSyBTaiIw1dfdG093204BKVHUurLy7Otu3BI",
   
-    authDomain: "g-tech-inventory.firebaseapp.com",
+  //   authDomain: "g-tech-inventory.firebaseapp.com",
   
-    projectId: "g-tech-inventory",
+  //   projectId: "g-tech-inventory",
   
-    storageBucket: "g-tech-inventory.appspot.com",
+  //   storageBucket: "g-tech-inventory.appspot.com",
   
-    messagingSenderId: "369201313814",
+  //   messagingSenderId: "369201313814",
   
-    appId: "1:369201313814:web:053f282e4bdc51756fdd37",
+  //   appId: "1:369201313814:web:053f282e4bdc51756fdd37",
   
-    measurementId: "G-79DPDERWZD"
+  //   measurementId: "G-79DPDERWZD"
   
-  }
+  // }
 };
 
 /*
