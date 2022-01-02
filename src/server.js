4aa29738056f668c372ada96cfa1aa259653ba64
Andrew-1000 <andruambia95@gmail.com>
2022-01-02 14:09:00 +0300

    ser.js file changed config.port

--- src/server.js
+++ src/server.js
@@ -13,5 +13,5 @@ app.get('/', function(req, res) {
 
 // Start the app by listening on the default Heroku port    
 app.listen(process.env.PORT || 3000, function() {
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+    console.log("Express server listening on port %d in %s mode", config.port, app.get('env'));
 });
