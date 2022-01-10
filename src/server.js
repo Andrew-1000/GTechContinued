09622651528eced4aff90ab4ad267a39cf6488b6
Andrew-1000 <andruambia95@gmail.com>
2022-01-10 23:17:31 +0300

    path changed

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static(__dirname + ''));
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + 'index.html'));
 });
 
 // Start the app by listening on the default Heroku port    
