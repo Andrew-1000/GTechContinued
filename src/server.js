26b3bccea0aeef3432c72a52d747306b84ea67ff
Andrew-1000 <andruambia95@gmail.com>
2022-01-10 20:49:14 +0300

    folder path changed

--- src/server.js
+++ src/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory    
-app.use(express.static(__dirname + '/dist/G-Tech-Continued'));
+app.use(express.static(__dirname + '/dist/'));
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/G-Tech-Continued/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port    
