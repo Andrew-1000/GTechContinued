f264529836c7e2b6514826437e43350ba97a75f2
Andrew-1000 <andruambia95@gmail.com>
2022-01-10 18:09:56 +0300

    server method (listen) only logs a message

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory    
+app.use(express.static(''));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join('index.html'));
+});
+
+// Start the app by listening on the default Heroku port    
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Express server listening on port %d in %s mode");
+});
